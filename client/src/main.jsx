bd0b4788d00be51c2f687d7ddfc326e6cedba5e0
oskarbayy <oskarbayy@gmail.com>
2025-04-01 10:27:28 +0200

    connect to superbase and setup server

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://uioawrrgevzolgdniqwz.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
