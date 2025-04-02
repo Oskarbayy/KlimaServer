396770b098a4dc8f9dac7f147700258d95a032e0
oskarbayy <oskarbayy@gmail.com>
2025-04-02 12:20:11 +0200

    Setup dynamic port

--- server/Program.cs
+++ server/Program.cs
@@ -3,9 +3,11 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 app.MapControllers();
-
-// Get the port from the environment (Railway sets it) or default to 3000
-var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
 app.MapGet("/", () => "API is running");
+
+// Get the port from the environment variable "PORT".
+// If it's not set, throw an exception.
+var port = Environment.GetEnvironmentVariable("PORT")
+           ?? throw new Exception("PORT environment variable not set.");
 
 app.Run($"http://0.0.0.0:{port}");
