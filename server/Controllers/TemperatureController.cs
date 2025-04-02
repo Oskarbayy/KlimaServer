f1df6a5e955022543e7515a48256ec9f62f66cb9
oskarbayy <oskarbayy@gmail.com>
2025-04-02 09:35:59 +0200

    changes test endpoint

--- server/Controllers/TemperatureController.cs
+++ server/Controllers/TemperatureController.cs
@@ -7,9 +7,7 @@ public class TemperatureController : ControllerBase
     [HttpPost]
     public IActionResult ReceiveTemperature([FromBody] TemperatureInput input)
     {
-        // Just log or respond for now — no DB
-        Console.WriteLine($"Temperature from {input.DeviceId}: {input.Value} °C");
-
+        Console.WriteLine($"Temperature from {input.DeviceId}: {input.Value}");
         return Ok(new
         {
             message = "Temperature received",
