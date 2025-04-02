8a642d701860926694ec533404019f886b10328c
oskarbayy <oskarbayy@gmail.com>
2025-04-02 08:34:41 +0200

    move nixpacks to server/

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e951814ee9f2011fe4b33554519a95d9458090")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa3ae5e33190b67308b564501511a20e85459b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
