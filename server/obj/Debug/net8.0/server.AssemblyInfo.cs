13f6747a20dedda325476252f0aa60cb6da1dfbb
oskarbayy <oskarbayy@gmail.com>
2025-04-04 09:39:27 +0200

    changed back to .net 8 .net 7 not work

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04680e03950ebe188182c86d422ef5b6f685f3cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49f224597386ff87d1cbf00e2359663ae6270a5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
