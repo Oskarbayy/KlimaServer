e617421cd2470543c7bb57a50646d9d88438cbdc
oskarbayy <oskarbayy@gmail.com>
2025-04-04 08:51:34 +0200

    test server to database working non locally

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5edc2d2e8fc8f5854941aacfea4f0b073dec3170")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb60737f681de649338d9871ebc651ce5c37dcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
