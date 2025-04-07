910e8e6d74452ac67f9dedecee091c0ab0f36527
oskarbayy <oskarbayy@gmail.com>
2025-04-07 12:10:05 +0200

    Added server new database connection test to see if it works in railway on host

--- server/Models/Arduino.cs
+++ server/Models/Arduino.cs
@@ -5,10 +5,15 @@ namespace Models
     [Table("arduinos")]
     public class Arduino
     {
+        [Column("id")]
         public int Id { get; set; }
-        public string Location { get; set; }
-        public string DeviceId { get; set; }
-        public ICollection<TempReading> TemperatureReadings { get; set; }
+
+        [Column("location")]
+        public string Location { get; set; } = null!;
+
+        [Column("device_id")]
+        public string DeviceId { get; set; } = null!;
+
+        public ICollection<TempReading> TemperatureReadings { get; set; } = new List<TempReading>();
     }
 }
-
--- server/Models/TempReading.cs
+++ server/Models/TempReading.cs
@@ -5,10 +5,18 @@ namespace Models
     [Table("temp")]
     public class TempReading
     {
+        [Column("id")]
         public int Id { get; set; }
+
+        [Column("value")]
         public double Value { get; set; }
+
+        [Column("timestamp")]
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+        [Column("arduino_id")]
         public int ArduinoId { get; set; }
-        public Arduino Arduino { get; set; }
+
+        public Arduino Arduino { get; set; } = null!;
     }
 }
