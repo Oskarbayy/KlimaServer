a4bb443b1cb2bdbe0069e9006f01b0ad13b4a517
oskarbayy <oskarbayy@gmail.com>
2025-04-09 09:09:55 +0200

    Added remove database entries over a week ago on every receive request

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3841aea6fb5c488304a4fe85623cd8aac9d18d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45468b62ffb77d4552673550e179401406cca896")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
